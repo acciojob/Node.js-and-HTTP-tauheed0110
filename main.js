9db9d6bdea7a005549490a492a136a2f574bc15d
saif0110 <khanmohdtauheed4@gmail.com>
2025-01-01 12:16:38 +0530

    file added

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   // TODO: Implement this function
+  let details= {
+    method: req.method,
+    url: req.url,
+    headers: req.headers
+  }
+  if(req.method === 'GET'){
+    res.end(JSON.stringify(details));
+  }
+  if(req.method==='POST'){
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+    details.body = body ? JSON.parse(body) : {};
+    req.on('end', ()=>{
+      res.end(JSON.stringify(details));
+    })
+  }
 });
 
 server.listen(3000, () => {
